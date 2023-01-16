50f7d657778f99a8dd4e41a07b4987dda7010655
dnzma13 <dnzma13@naver.com>
2023-01-16 12:20:16 +0900

    BE:Feat(info): add getPhrases Api - add getPhrases Api - add getPhrases Test code

--- SHabit-back/src/main/java/com/ezpz/shabit/info/controller/InfoController.java
+++ SHabit-back/src/main/java/com/ezpz/shabit/info/controller/InfoController.java
@@ -1,8 +1,6 @@
 package com.ezpz.shabit.info.controller;
 
 import com.ezpz.shabit.info.dto.res.PhrasesResDto;
-import com.ezpz.shabit.info.dto.res.VodResDto;
-import com.ezpz.shabit.info.entity.Phrases;
 import com.ezpz.shabit.info.service.InfoService;
 import com.ezpz.shabit.util.Response;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -22,19 +19,6 @@ import java.util.List;
 public class InfoController {
 
   private final InfoService infoService;
-
-  // 랜덤 영상 조회 API
-  @GetMapping("vods")
-  public ResponseEntity<?> getVodList() {
-    try {
-      List<VodResDto> result = infoService.getVodList();
-      log.info("vodList : {} ", result);
-      return Response.makeResponse(HttpStatus.OK, "영상 가져오기 성공", result.size(), result);
-    } catch (Exception e) {
-      log.info("error : {}", e.getClass());
-      return Response.notFound("영상 가져오기 실패");
-    }
-  }
 
   // 건강 문구 랜덤 조회 API
   @GetMapping("phrases")
--- SHabit-back/src/main/java/com/ezpz/shabit/user/controller/UserController.java
+++ SHabit-back/src/main/java/com/ezpz/shabit/user/controller/UserController.java
@@ -1,7 +1,17 @@
 package com.ezpz.shabit.user.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/")
+@Slf4j
+@RequiredArgsConstructor
 public class UserController {
+
 }
