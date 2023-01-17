39fea57a8db5f65db43180157061efbda29e3a99
swj <159632_@naver.com>
2023-01-17 23:05:08 +0900

    Test : 로그인 테스트 코드 파일 생성

---
+++ SHabit-back/src/test/java/com/ezpz/shabit/user/controller/UserControllerTest.java
@@ -0,0 +1,24 @@
+package com.ezpz.shabit.user.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * author : cadqe13@gmail.com
+ * date : 2023-01-17
+ * description :
+ */
+
+@SpringBootTest
+class UserControllerTest {
+
+	@Test
+	void signUp() {
+	}
+
+	@Test
+	void login() {
+	}
+}
---
+++ SHabit-back/src/test/java/com/ezpz/shabit/user/service/UserServiceImplTest.java
@@ -0,0 +1,22 @@
+package com.ezpz.shabit.user.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * author : cadqe13@gmail.com
+ * date : 2023-01-17
+ * description :
+ */
+
+class UserServiceImplTest {
+
+	@Test
+	void signUp() {
+	}
+
+	@Test
+	void login() {
+	}
+}
