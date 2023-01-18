9a7106898692fa5541d59a27208f902103db419c
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2023-01-18 17:44:10 +0900

    FE:Feat: Reset Password

--- shabit-front/src/components/authTest/LoginForm.jsx
+++ shabit-front/src/components/authTest/LoginForm.jsx
@@ -1,12 +1,41 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 const LoginForm = () => {
   const [inputs, setInputs] = useState({
     email: '',
     password: '',
   });
-  const [message, setMessage] = useState('');
+  const [message, setCurrentMessage] = useState('');
+  const setMessage = (str) => {
+    setCurrentMessage(str);
+    setTimeout(() => {
+      setCurrentMessage('');
+    }, 2000);
+  };
   const { email, password } = inputs;
+  const [isResetPassword, setIsResetPassword] = useState(false);
+  const toggleReset = (e) => {
+    e.preventDefault();
+    setIsResetPassword(!isResetPassword);
+  };
+
+  const onReset = (e) => {
+    e.preventDefault();
+    api.put(`/user/${email}`).then(() => {
+      setMessage('비밀번호가 초기화되었습니다');
+      setIsResetPassword(false);
+    });
+  };
+
+  // const [autoLogin, setAutoLogin] = useState(false)
+  /**
+   * 자동 로그인 로직
+   * 1. 자동 로그인이 체크되면 setAutoLogin을 true로 한다.
+   * 2. 로그인 요청을 보내고 응답을 받는다.
+   * 3. 받은 응답을 처리할 때에, autoLogin이 true이면 새로운 쿠키(autoLoginAccessToken)???를 만든다.
+   *
+   * 0. useEffect를 이용해서, 최초 로그인 시, autoLoginAccessToken을 조회해서 값이 있으면 해당 토큰으로 요청을 보낸다.
+   */
 
   const onChangeHandler = (e) => {
     const { value, name } = e.target;
@@ -68,31 +97,53 @@ const LoginForm = () => {
   return (
     <div>
       {message}
-      <form style={{ width: '600px' }} onSubmit={onSubmitHandler}>
-        <br />
-        <label htmlFor="emailInput">email</label>
-        <input
-          type="email"
-          name="email"
-          id="emailInput"
-          placeholder="name@example.com"
-          value={email}
-          onChange={onChangeHandler}
-        />
+      {!isResetPassword ? (
+        <>
+          <form style={{ width: '600px' }} onSubmit={onSubmitHandler}>
+            <br />
+            <label htmlFor="emailInput">email</label>
+            <input
+              type="email"
+              name="email"
+              id="emailInput"
+              placeholder="name@example.com"
+              value={email}
+              onChange={onChangeHandler}
+            />
 
-        <br />
-        <label htmlFor="passwordInput">password</label>
-        <input
-          type="password"
-          name="password"
-          id="passwordInput"
-          placeholder="password"
-          value={password}
-          onChange={onChangeHandler}
-        />
-        <br />
-        <button type="submit">로그인</button>
-      </form>
+            <br />
+            <label htmlFor="passwordInput">password</label>
+            <input
+              type="password"
+              name="password"
+              id="passwordInput"
+              placeholder="password"
+              value={password}
+              onChange={onChangeHandler}
+            />
+            <br />
+            <button type="submit">로그인</button>
+          </form>
+          <p onClick={toggleReset}>비밀번호를 잊으셨나요?</p>
+        </>
+      ) : (
+        <>
+          <form onSubmit={onReset}>
+            <label htmlFor="emailInput">email</label>
+            <input
+              type="email"
+              name="email"
+              id="emailInputforReset"
+              placeholder="name@example.com"
+              value={email}
+              onChange={onChangeHandler}
+            />
+            <br />
+            <button type="submit">비밀번호 초기화하기</button>
+          </form>
+          <p onClick={toggleReset}>로그인하러 가기</p>
+        </>
+      )}
     </div>
   );
 };
