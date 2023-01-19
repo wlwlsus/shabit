27be970a2c227a908a3002d2bd60d021149fe213
dnzma13 <dnzma13@naver.com>
2023-01-19 14:20:48 +0900

    initial commit

--- SHabit-back/src/main/java/com/ezpz/shabit/info/controller/InfoController.java
+++ SHabit-back/src/main/java/com/ezpz/shabit/info/controller/InfoController.java
@@ -1,18 +1,9 @@
 package com.ezpz.shabit.info.controller;
 
-import com.ezpz.shabit.info.dto.CategoryResDto;
-import com.ezpz.shabit.info.entity.Category;
-import com.ezpz.shabit.info.service.InfoService;
-import com.ezpz.shabit.util.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 
 @RestController
@@ -21,18 +12,5 @@ import java.util.List;
 @RequiredArgsConstructor
 public class InfoController {
 
-  private final InfoService infoService;
-
-  @GetMapping("/category")
-  public ResponseEntity<?> getCategoryList() {
-    try {
-      List<CategoryResDto> result = infoService.getCategoryList();
-      log.info("category list : {}", result);
-      log.info("get categoryList successfully");
-      return Response.makeResponse(HttpStatus.OK, "카테고리 목록 반환 성공", result.size(), result);
-    } catch (Exception e) {
-      return Response.badRequest("카테고리 목록 반환 실패");
-    }
-  }
 
 }
--- SHabit-back/src/main/java/com/ezpz/shabit/info/service/InfoServiceImpl.java
+++ SHabit-back/src/main/java/com/ezpz/shabit/info/service/InfoServiceImpl.java
@@ -1,6 +1,5 @@
 package com.ezpz.shabit.info.service;
 
-import com.ezpz.shabit.info.dto.CategoryResDto;
 import com.ezpz.shabit.info.entity.Category;
 import com.ezpz.shabit.info.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,13 +12,4 @@ import java.util.List;
 @Slf4j
 @RequiredArgsConstructor
 public class InfoServiceImpl implements InfoService {
-  private final CategoryRepository categoryRepository;
-
-  @Override
-  public List<CategoryResDto> getCategoryList() throws Exception {
-    List<Category> list = categoryRepository.findAll();
-    log.info("list in service : {}", list);
-    List<CategoryResDto> result = list.stream().map((CategoryResDto::new)).toList();
-    return result;
-  }
 }
