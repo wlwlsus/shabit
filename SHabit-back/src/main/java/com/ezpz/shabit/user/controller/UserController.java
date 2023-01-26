26751a09947fed3d9ccd5d826f7d303902f2e17d
wlwlsus <cadqe13@gmail.com>
2023-01-26 16:04:13 +0900

    BE: Fix : Modify Folder Path

--- SHabit-back/src/main/java/com/ezpz/shabit/user/controller/UserController.java
+++ SHabit-back/src/main/java/com/ezpz/shabit/user/controller/UserController.java
@@ -1,25 +1,21 @@
 package com.ezpz.shabit.user.controller;
 
-import com.ezpz.shabit.user.dto.req.UserTestReqDto;
-import com.ezpz.shabit.user.service.EmailService;
 import com.ezpz.shabit.user.dto.req.UserReqDto;
-import com.ezpz.shabit.user.service.S3File;
+import com.ezpz.shabit.user.dto.req.UserTestReqDto;
+import com.ezpz.shabit.user.dto.res.UserTestResDto;
+import com.ezpz.shabit.user.service.EmailService;
 import com.ezpz.shabit.user.service.UserService;
 import com.ezpz.shabit.util.Response;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.ezpz.shabit.user.dto.res.UserTestResDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -39,7 +35,7 @@ public class UserController {
   private final EmailService emailService;
 
   // 프로필 사진 변경 API
-  @PutMapping("/profile/{email}")
+  @PutMapping(path = "/profile/{email}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<?> updateProfile(@RequestBody MultipartFile profile, @PathVariable String email) {
     try {
       Map<String, String> result = new HashMap<>();
@@ -174,8 +170,8 @@ public class UserController {
 
 
   @Operation(description = "회원가입 API", responses = {
-          @ApiResponse(responseCode = "200", description = "회원가입 성공"),
-          @ApiResponse(responseCode = "400", description = "회원가입 실패"),
+      @ApiResponse(responseCode = "200", description = "회원가입 성공"),
+      @ApiResponse(responseCode = "400", description = "회원가입 실패"),
   })
   @PostMapping("")
   public ResponseEntity<?> signUp(@RequestBody @Validated UserTestReqDto.SignUp signUp, Errors errors) {
@@ -189,9 +185,9 @@ public class UserController {
   }
 
   @Operation(description = "로그인 API", responses = {
-          @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(schema =
-          @Schema(implementation = UserTestResDto.UserInfo.class))),
-          @ApiResponse(responseCode = "400", description = "로그인 실패"),
+      @ApiResponse(responseCode = "200", description = "로그인 성공", content = @Content(schema =
+      @Schema(implementation = UserTestResDto.UserInfo.class))),
+      @ApiResponse(responseCode = "400", description = "로그인 실패"),
   })
   @PostMapping("/login")
   public ResponseEntity<?> login(@RequestBody @Validated UserTestReqDto.Login login, Errors errors) {
@@ -205,8 +201,8 @@ public class UserController {
   }
 
   @Operation(description = "로그아웃 API", responses = {
-          @ApiResponse(responseCode = "200", description = "로그아웃 성공"),
-          @ApiResponse(responseCode = "400", description = "로그아웃 실패"),
+      @ApiResponse(responseCode = "200", description = "로그아웃 성공"),
+      @ApiResponse(responseCode = "400", description = "로그아웃 실패"),
   })
   @PostMapping("/logout")
   public ResponseEntity<?> logout(@RequestBody @Validated UserTestReqDto.Logout logout, Errors errors) {
@@ -217,9 +213,9 @@ public class UserController {
   }
 
   @Operation(description = "토큰 재발급 API", responses = {
-          @ApiResponse(responseCode = "200", description = "토큰 재발급 성공", content = @Content(schema =
-          @Schema(implementation = UserTestResDto.TokenInfo.class))),
-          @ApiResponse(responseCode = "400", description = "토큰 재발급 실패"),
+      @ApiResponse(responseCode = "200", description = "토큰 재발급 성공", content = @Content(schema =
+      @Schema(implementation = UserTestResDto.TokenInfo.class))),
+      @ApiResponse(responseCode = "400", description = "토큰 재발급 실패"),
   })
   @PostMapping("/token")
   public ResponseEntity<?> reissue(@RequestBody @Validated UserTestReqDto.Reissue reissue, Errors errors) {
@@ -231,9 +227,9 @@ public class UserController {
   }
 
   @Operation(description = "회원 정보 API", responses = {
-          @ApiResponse(responseCode = "200", description = "회원 정보 요청 성공", content = @Content(schema =
-          @Schema(implementation = UserTestResDto.LoginUserRes.class))),
-          @ApiResponse(responseCode = "400", description = "회원 정보 요청 실패"),
+      @ApiResponse(responseCode = "200", description = "회원 정보 요청 성공", content = @Content(schema =
+      @Schema(implementation = UserTestResDto.LoginUserRes.class))),
+      @ApiResponse(responseCode = "400", description = "회원 정보 요청 실패"),
   })
 
   @GetMapping("/{email}")
