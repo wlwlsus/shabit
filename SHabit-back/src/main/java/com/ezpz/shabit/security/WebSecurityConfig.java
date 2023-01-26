6a859250f84870daf89f389d4f6cebb5f4ad0338
wlwlsus <cadqe13@gmail.com>
2023-01-26 09:40:44 +0900

    BE : Refactor : Modify Security Access Permissions

--- SHabit-back/src/main/java/com/ezpz/shabit/security/WebSecurityConfig.java
+++ SHabit-back/src/main/java/com/ezpz/shabit/security/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig {
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
         .authorizeHttpRequests()
-        .requestMatchers("/api/v1/user", "/api/v1/user/login", "/api/v1/user/logout", "/api/v1/user/token", "/swagger-ui/**", "/v3/api" +
+        .requestMatchers("/api/v1/user/password-find/**", "/api/v1/user/email-check/**", "/api/v1/user/email-valid/**", "/api/v1/user", "/api/v1/user/login", "/api/v1/user/logout", "/api/v1/user/token", "/swagger-ui/**", "/v3/api" +
             "-docs/**").permitAll()
         .requestMatchers("/**").hasRole("USER")
         .and()
