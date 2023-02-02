1d7cf5cfb562e4263d2cc59cafc04e2f38156df6
석민지 <mjseok98@gmail.com>
2023-02-02 13:05:09 +0900

    FE:FEAT Timer

--- shabit-front/src/components/TeachableMachineTest/MyPose.jsx
+++ shabit-front/src/components/TeachableMachineTest/MyPose.jsx
@@ -5,63 +5,65 @@ import Loading from '../common/Loading';
 const MyPose = ({
   isStarting,
   setIsStarting,
-  savedIntevalId,
   timerIntervalId,
+  savedIntevalId
 }) => {
 
-  let model, webcam, ctx , maxPredictions;
+  let model, webcam ,ctx, maxPredictions;
   const canvasREF = useRef();
-  let fisrtTime;
 
   //이전 데이터를 저장할 전역변수를 만듭니다.
   let prevMaxClass = '';
   let maxTime = '';
   let preparedLog = {};
-  let prevMaxPrediction = 0;
-  let isRunning = true;
 
   const [isLoading, setIsLoading] = useState(false); // 로딩중이면 스핀휠을 호출
   const [maxClassState, setMaxClassState] = useState(''); // 비율이 가장 높은 자세
   const [maxPredictionState, setMaxPredictionState] = useState(''); // 가장 높은 자세의 비율
   const [logArray, setLogArray] = useState([]); // 로그를 배열로 기록함.
-  const [travelTime, setTravelTime] = useState(0);
-  const [remainTime, setRemainTime] = useState(3000000);
-  const [strechingCount, setStrechingCount] = useState(1);
+  const [remainMin, setRemainMin] = useState(50);
+  const [remainSec, setRemainSec] = useState(0);
 
-  const displayHours =
-    Math.floor((travelTime / (1000 * 60 * 60)) % 24) > 0
-      ? String(Math.floor((travelTime / (1000 * 60 * 60)) % 24)).padStart(
-          2,
-          '0',
-        ) + ':'
-      : '';
-  const displayTime =
-    displayHours +
-    String(Math.floor((travelTime / (1000 * 60)) % 60)).padStart(2, '0') +
-    ':' +
-    String(Math.floor((travelTime / 1000) % 60)).padStart(2, '0');
+  const [usedHour, setUsedHour] = useState(0);
+  const [usedMin, setUsedMin] = useState(0);
 
-  const displayRemainTime =
-    String(Math.floor((remainTime / (1000 * 60)) % 60)).padStart(2, '0') +
-    ':' +
-    String(Math.floor((remainTime / 1000) % 60)).padStart(2, '0');
+  const [strechingCount, setStrechingCount] = useState(0);
+  useEffect(()=>{
+    const remainTime = setInterval(()=>{
+      if(remainSec>0) setRemainSec(remainSec-1);
+      else if(remainSec===0  && remainMin===0) clearInterval(remainTime);
+      else if(remainSec===0 && remainMin!==0) {
+        setRemainSec(59);
+        setRemainMin(remainMin-1);
+      }
+    },1000);
+    
+    return ()=>clearInterval(remainTime);
+  },[remainMin,remainSec])
 
+  useEffect(()=>{
+    const usedTime = setInterval(()=>{
+      if(usedMin===59) {
+        setUsedMin(0);
+        setUsedHour(usedHour+1);
+      }
+      else setUsedMin(usedMin+1);
+    },60000);
+    return ()=>clearInterval(usedTime);
+  },[usedHour,usedMin])
+
+  const displayRemainTime = `${remainMin}:${remainSec}`;
+  const displayUsedTime = `${usedHour}:${usedMin}`;
+    
   useEffect(() => {
-    const fiftyMinutes = 3000000;
-    const offset = 990; // math.floor로 해도 작동 되도록
-    setRemainTime(offset + fiftyMinutes * strechingCount - travelTime);
-  }, [travelTime, strechingCount]);
-
-  useEffect(() => {
-    if (remainTime <= 0) {
+    if (remainMin === 0 && remainSec===0) {
       setStrechingCount(strechingCount + 1);
     }
-  }, [remainTime]);
+  }, [remainMin,remainSec]);
 
   // TM: 정지 버튼을 눌렀을 때에 intervalID를 기준으로 loop함수를 중단합니다.
   async function onStop() {
     const jsonData = JSON.stringify(logArray);
-    isRunning = false;
     await localStorage.setItem('data', jsonData);
     await clearInterval(savedIntevalId.current);
     await clearInterval(timerIntervalId.current);
@@ -87,12 +89,8 @@ const MyPose = ({
     await webcam.setup(); // request access to the webcam
     await webcam.play();
     setIsLoading(false);
-    fisrtTime = new Date();
+
     savedIntevalId.current = setInterval(loop, 16);
-    timerIntervalId.current = setInterval(() => {
-      const now = new Date();
-      setTravelTime(now - fisrtTime);
-    }, 495);
 
     // append/get elements to the DOM
     const canvas = canvasREF.current;
@@ -120,9 +118,6 @@ const MyPose = ({
     let maxPrediction = 0;
 
     for (let i = 0; i < maxPredictions; i++) {
-      const classPrediction =
-        prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
-
       //렌더링을 진행하는 알고리즘에 최대값 비교를 추가하였습니다.
       if (prediction[i].probability.toFixed(2) > maxPrediction) {
         //probability가 가장 큰 값이 maxPrediction에 저장되며 그 클래스명이 maxClass에 저장됩니다.
@@ -130,13 +125,12 @@ const MyPose = ({
         maxClass = prediction[i].className;
       }
     }
-
-    // //콘솔을 찍는 알고리즘입니다.
+    // TODO: 알람으로 바꿔야함
+    // //콘솔을 찍는 알고리즘입니다. -> 알람으로 바꿔야함
     // //전역변수와 비교하여 maxClass값이 변경되었을 경우,
     // //혹은 maxPrediction값이 0.1 이상 바뀌거나 1에 도달한 경우에 로그를 남깁니다.
     if (prevMaxClass !== maxClass && maxPrediction >= 0.7) {
       const now = new Date();
-      // const now = Date.now();
       const timeArray = [
         now.getFullYear().toString(),
         '-',
@@ -152,7 +146,6 @@ const MyPose = ({
       ];
       const endTime = timeArray.join('');
       prevMaxClass = maxClass;
-      prevMaxPrediction = maxPrediction;
       setMaxClassState(maxClass);
       setMaxPredictionState(maxPrediction);
       console.log('새시간', endTime);
@@ -189,8 +182,9 @@ const MyPose = ({
       <button type="button" onClick={init}>
         Start
       </button>
-      <div>{displayTime} 총 이용 시간</div>
+      <div>{displayUsedTime} 총 이용 시간</div>
       <div>{displayRemainTime} 스트레칭까지 남은 시간</div>
+      <div>스트레칭 횟수 : {strechingCount}</div>
       <button type="button" onClick={onStop}>
         Stop
       </button>
