dc297c4518c9357c2d75bb145a591c1b59bd6bde
고수연[서울_6반_A601]팀원 <kosy1782@gmail.com>
2023-02-03 13:18:18 +0900

    BE:Refactor(admin): change to delete by content

--- SHabit-back/src/test/java/com/ezpz/shabit/AdminControllerTest.java
+++ SHabit-back/src/test/java/com/ezpz/shabit/AdminControllerTest.java
@@ -3,7 +3,9 @@ package com.ezpz.shabit;
 import com.ezpz.shabit.admin.controller.AdminController;
 import com.ezpz.shabit.admin.dto.res.SettingResDto;
 import com.ezpz.shabit.admin.service.AdminServiceImpl;
+import com.ezpz.shabit.info.dto.req.PhrasesReqDto;
 import com.ezpz.shabit.info.entity.Category;
+import com.ezpz.shabit.info.entity.Phrases;
 import com.ezpz.shabit.info.entity.Vod;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,6 +28,7 @@ import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class) // @WebMVCTest를 이용할 수도 있지만 속도가 느리다
@@ -97,8 +100,6 @@ public class AdminControllerTest {
         }
         return vodIdList;
     }
-
-
 
 //    @Test
 //    void 영상_입력된_길이로_목록_조회_성공() throws Exception{
@@ -213,5 +214,129 @@ public class AdminControllerTest {
         MvcResult mvcResult = resultActions.andExpect(status().isOk()).andReturn();
         System.out.println(mvcResult.getResponse().getContentAsString());
     }
+
+    @Test
+    public void 없는_건강_문구_삭제_실패() throws Exception {
+        // given
+        List<String> list = phrasesContentList();
+        doThrow(new NullPointerException("존재하지않는 문구 입니다.")).when(adminService)
+                .deletePhrases(list);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.delete("/api/v1/admin/phrase")
+                        .content(gson.toJson(list))
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        // HTTP Status가 OK인지 확인
+        MvcResult mvcResult = resultActions.andExpect(status().isBadRequest()).andReturn();
+
+        // 주어진 데이터가 올바른지 검증해야하는데 Json 응답을 객체로 변환하여 확인
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
+    void 건강_문구_삭제_성공() throws Exception{
+        // given
+        List<String> list = phrasesContentList();
+        doReturn(3).when(adminService)
+                .deletePhrases(list);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.delete("/api/v1/admin/phrase")
+                        .content(gson.toJson(list))
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        // HTTP Status가 OK인지 확인
+        MvcResult mvcResult = resultActions.andExpect(status().isOk()).andReturn();
+
+        // 주어진 데이터가 올바른지 검증해야하는데 Json 응답을 객체로 변환하여 확인
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+
+    private List<String> phrasesContentList() {
+        List<String> phrasesContentList = new ArrayList<>();
+        for(int i=0; i<3; i++){
+            phrasesContentList.add(Integer.toString(i+1));
+        }
+        return phrasesContentList;
+    }
+
+//    @Test
+//    void 건강_문구_목록_조회_성공() throws Exception{
+//        // given
+//        // findAll에 대한 stub필요
+//        doReturn(phrasesList()).when(adminService)
+//                .getPhrasesList();
+//
+//        // when
+//        ResultActions resultActions = mockMvc.perform(
+//                MockMvcRequestBuilders.get("/api/v1/admin/phrase")
+//        );
+//
+//        // then
+//        // HTTP Status가 OK인지 확인
+//        MvcResult mvcResult = resultActions.andExpect(status().isOk()).andReturn();
+//
+//        // 주어진 데이터가 올바른지 검증해야하는데 Json 응답을 객체로 변환하여 확인
+//        System.out.println(mvcResult.getResponse().getContentAsString());
+//    }
+
+    @Test
+    public void 건강_문구_중복() throws Exception {
+        // given
+        final String url = "/api/v1/admin/phrase";
+        PhrasesReqDto req = PhrasesReqDto.builder().content("허리 피세여").build();
+        doReturn(0).when(adminService)
+                .insertPhrases(any(PhrasesReqDto.class));
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .content(new Gson().toJson(req))
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        // HTTP Status가 NotFound인지 확인
+        resultActions.andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void 건강_문구_추가_성공() throws Exception {
+        // given
+        final String url = "/api/v1/admin/phrase";
+        PhrasesReqDto req = PhrasesReqDto.builder().content("허리 피세여").build();
+        doReturn(1).when(adminService)
+                .insertPhrases(any(PhrasesReqDto.class));
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new Gson().toJson(req))
+        );
+
+        // then
+        // HTTP Status가 OK인지 확인
+        MvcResult mvcResult = resultActions.andExpect(status().isOk()).andReturn();
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+
+    private List<Phrases> phrasesList() {
+        List<Phrases> phrasesList = new ArrayList<>();
+        for(int i=0; i<3; i++){
+            phrasesList.add(Phrases.builder()
+                    .content("허리피세여" + Integer.toString(i))
+                    .build());
+        }
+        return phrasesList;
+    }
+
 
 }
