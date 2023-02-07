b51c73714c8a5443c213c40a734d08a2c67236ae
swj <159632_@naver.com>
2023-02-07 21:25:22 +0900

    Fix : shell script

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 docker-compose pull
 
-COMPOSE_DOCKER_CLI_BUILD=1 
-
-DOCKER_BUILDKIT=1 
-
+# COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 
 docker-compose up --build -d
 
 docker rmi -f $(docker images -f "dangling=true" -q) || true
