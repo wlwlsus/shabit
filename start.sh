8802fdceeda2d4eca59370dac7b73cb6b57f087e
swj <159632_@naver.com>
2023-02-07 21:28:58 +0900

    Fix : shell script

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
+git clone https://lab.ssafy.com/s08-webmobile1-sub2/S08P12A601.git
+cd S08P12A601
 docker-compose pull
 
-# COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 
-docker-compose up --build -d
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose up --build -d
 
 docker rmi -f $(docker images -f "dangling=true" -q) || true
