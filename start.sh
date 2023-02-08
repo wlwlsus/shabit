677e79306b8f977c81d21a63db7cb02214560fa0
wlwlsus <cadqe13@gmail.com>
2023-02-08 14:10:25 +0900

    Feat : Add Dev Env & Prod Env

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-docker-compose pull
+docker-compose -f docker-compose-prod.yml pull
 
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose up --build -d
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose-prod.yml up --build -d
 
 docker rmi -f $(docker images -f "dangling=true" -q) || true
