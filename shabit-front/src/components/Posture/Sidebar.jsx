828302dde20d74576b7e3ec7e471b20007c6a683
wlwlsus <cadqe13@gmail.com>
2023-02-16 08:55:25 +0900

    FE:Fix: Solve Conflict

--- shabit-front/src/components/Posture/Sidebar.jsx
+++ shabit-front/src/components/Posture/Sidebar.jsx
@@ -60,15 +60,9 @@ const Sidebar = () => {
       dispatch(setStretchModal(true));
     }
   }, [stretchingMin, stretchingSec]);
-<<<<<<< HEAD
   useEffect(() => {
     dispatch(setInitStretchingTime(initStretchingMin));
   }, []);
-=======
-  useEffect(()=>{
-    dispatch(setInitStretchingTime(initStretchingMin));
-  },[]);
->>>>>>> dev-front
   const usedTime = useSelector((state) => {
     return `${state.time.usedTime.hour}:${state.time.usedTime.min}`;
   });
@@ -133,39 +127,24 @@ const Sidebar = () => {
         </TimeContainer>
       )}
       <CapturingContainer>
-<<<<<<< HEAD
         {toggle && curPose && (
-=======
-        {(toggle&&curPose)&&(
->>>>>>> dev-front
           <IconWrapper>
             <CgPlayPause onClick={clickPauseButton} />
             <Text>일시정지</Text>
           </IconWrapper>
-<<<<<<< HEAD
         )}
         {!toggle && curPose && (
-=======
-        )}{(!toggle&&curPose)&&(
->>>>>>> dev-front
           <IconWrapper>
             <CgPlayButton onClick={clickPlayButton} />
             <Text>시작</Text>
           </IconWrapper>
         )}
-<<<<<<< HEAD
         {curPose && (
           <IconWrapper>
             <ImExit onClick={clickStop} />
             <Text>종료하기</Text>
           </IconWrapper>
         )}
-=======
-        {curPose&&<IconWrapper>
-          <ImExit onClick={clickStop} />
-          <Text>종료하기</Text>
-        </IconWrapper>}
->>>>>>> dev-front
       </CapturingContainer>
     </ContainerWrapper>
   );
